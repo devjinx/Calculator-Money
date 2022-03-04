26526b49ea108e754809b1b6b435aad3a9313630
정 석진 <73208494+devjinx@users.noreply.github.com>
2022-03-04 22:14:17 +0700

    Update patch 1.001
    add sometext for show

--- Main.c
+++ Main.c
@@ -5,5 +5,6 @@ void main()
    printf("Press Your Money You Wanna Keep");
    scanf("%d",&money);
    printf("Your money you wanna Keep %d \n",money);
+   printf("Thank for Use This Programe");
    system("pause");
    }
