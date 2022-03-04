5dca5c1884b72035ff215ab9eec59dade476ce0f
정 석진 <73208494+devjinx@users.noreply.github.com>
2022-03-04 17:28:18 +0700

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main()
+{
+    int money;
+   printf("Press Your Money You Wanna Keep");
+   scanf("%d",&money);
+   printf("Your money you wanna Keep %d \n",money);
+   system("pause");
+   }
