22c8476eea6de981054f02f2616c7ef180dfbef2
devjinx <jeongseokjin28@gmail.com>
2022-03-11 18:02:07 +0700

    Not ready for gui of python lang

---
+++ Main_Gui.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+root = Tk()
+root.title("Calculator-Moneyforkeep-1year")
+
+#text box
+txt=StringVar
+Money =Entry(root,textvariable=txt).pack()
+
+#button
+
+
+#about loop programe and edit size display
+root.geometry("500x500")
+root.mainloop()
