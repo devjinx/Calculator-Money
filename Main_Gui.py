2f273631dfbf2524ffff0fe43343e8b050ebc34b
devjinx <jeongseokjin28@gmail.com>
2022-03-28 12:55:56 +0700

    Already Programme But ui not good verison

--- Main_Gui.py
+++ Main_Gui.py
@@ -1,15 +1,25 @@
 from tkinter import *
+from tkinter import ttk
 
 root = Tk()
-root.title("Calculator-Moneyforkeep-1year")
+root.title("คำนวณการเก็บเงินภายใน 1 ปี")
 
-#text box
-txt=StringVar
-Money =Entry(root,textvariable=txt).pack()
+#input
+Label(text="จำนวนเงินที่คุณต้องการเก็บ", padx=10, font=30).grid(row=0, sticky=W)
+money = IntVar()
+et1=Entry(font=30,width=30, textvariable=money)
+et1.grid(row=0, column=1)
 
-#button
+#output
+Label(text="คุณควรเก็บเงิน", padx=10, font=30).grid(row=2, sticky=W)
+et2=Entry(font=30,width=30)
+et2.grid(row=2, column=1)
+def calculate():
+    m = money.get()
+    m1 = m / 12
+    et2.insert(0,m1)
 
+Button(text="คำนวณ", font=30,width=15, command=calculate).grid(row=3, column=1, sticky=W)
 
-#about loop programe and edit size display
 root.geometry("500x500")
 root.mainloop()
